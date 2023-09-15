691bf02a4f076816d9ff614b4082253100c69270
ikicamilo <ikicamilo@gmail.com>
2023-09-15 12:09:37 -0500

    cors is fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://auth-front-tx1t.onrender.com"],
     credentials: true,
   })
 );
